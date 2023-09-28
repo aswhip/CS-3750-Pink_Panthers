1201f207fbf0ac0dfd8f1713e72c8fd21be663c6
nathandavis18 <nathandavis8@mail.weber.edu>
2023-09-28 15:51:04 -0600

    Had to modify project solution placement

---
+++ Pink_Panthers_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Pink_Panthers_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TeacherCanCreateClass()
+        {
+            //Count how many classes teacher has (n classes)
+            //Create new class
+            //Add Class to Database
+            //Count how many classes teacher has (n + 1 classes)
+            //Delete class
+        }
+    }
+}
