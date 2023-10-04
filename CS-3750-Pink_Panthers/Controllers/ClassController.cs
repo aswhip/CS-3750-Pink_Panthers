9335f70fae2d311a36cfce4fcfc2a6417108ed8f
Wannes-Schepers <wannes.schepers@student.ucll.be>
2023-10-04 15:42:21 -0600

    Added Controller

---
+++ CS-3750-Pink_Panthers/Controllers/ClassController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pink_Panthers_Project.Data;
+using Pink_Panthers_Project.Models;
+
+namespace Pink_Panthers_Project.Controllers
+{
+    public class ClassController : Controller
+    {
+        private readonly Pink_Panthers_ProjectContext _context;
+
+
+        public ClassController(Pink_Panthers_ProjectContext context)
+        {
+            _context = context;
+        }
+
+        private static Account? _account;
+
+
+        public IActionResult Index(int id)
+        {
+            ViewBag.isTeacher = _account!.isTeacher;
+
+
+            var assignments = _context.Assignments.Where(a => a.ClassID == id);
+            var currentClass = _context.Class.Find(id);
+            var viewModel = new ClassViewModel
+            {
+                Class = currentClass,
+                Assignments = assignments.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
