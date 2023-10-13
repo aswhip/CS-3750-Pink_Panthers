73b73b0f2ba1c43d382193092c9715bbad2206bf
nathandavis18 <nathandavis8@mail.weber.edu>
2023-10-12 19:21:19 -0600

    Very Very (Very) basic Selenium Test using Edge browser (that's what I use)

---
+++ Pink_Panthers_Test/SeleniumTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace Pink_Panthers_Test
+{
+	[TestClass]
+	public class SeleniumTest
+	{
+		[TestMethod]
+		public void TestingSelenium()
+		{
+			IWebDriver driver = new EdgeDriver();
+			string url = "https://www.google.com";
+			driver.Manage().Window.Maximize();
+
+			driver.Navigate().GoToUrl(url);
+
+			driver.Quit();
+
+		}
+	}
+}
