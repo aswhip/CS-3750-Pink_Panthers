51eaa50d87bf54c2f64a906d7eb859ca5a2cff32
Nathan Davis <nathandavis8@mail.weber.edu>
2023-10-19 13:07:40 -0600

    Fixed Edit Class

--- SeleniumTests/RegisterDropTests.cs
+++ SeleniumTests/RegisterDropTests.cs
@@ -45,46 +45,18 @@ namespace SeleniumTests
 
             System.Threading.Thread.Sleep(4000); //Sleep for 4 seconds so we can see that the unit test course has been registered
 
+			registerLink = driver.FindElement(By.Id("registerLink"));
+			registerLink.Click();
 
-            driver.Quit();
+			System.Threading.Thread.Sleep(2000); //Sleep for 2 seconds to give the register screen time to load
 
-        }
+			unitTestCourse = driver.FindElement(By.Id("6dropBtn"));
+			unitTestCourse.Click();
 
-        [TestMethod]
-        public void ZDropRegisteredClassTest() //Alphabetical ordering with the Z
-        {
-			IWebDriver driver = new EdgeDriver();
-			//IWebDriver driver = new ChromeDriver();
-
-			string url = "https://pinkpanthers3750.azurewebsites.net";
-            driver.Manage().Window.Maximize();
-
-            driver.Navigate().GoToUrl(url);
-
-            IWebElement username = driver.FindElement(By.Id("usernameText"));
-            username.SendKeys("teststudent@gmail.com");
-
-            IWebElement password = driver.FindElement(By.Id("passwordText"));
-            password.SendKeys("test1");
-
-            IWebElement loginBtn = driver.FindElement(By.Id("loginBtn"));
-
-            loginBtn.Click();
-
-            System.Threading.Thread.Sleep(3000); //Sleep for 3 seconds to give the profile screen time to load
-
-            IWebElement registerLink = driver.FindElement(By.Id("registerLink"));
-            registerLink.Click();
-
-            System.Threading.Thread.Sleep(2000); //Sleep for 2 seconds to give the register screen time to load
-
-            IWebElement unitTestCourse = driver.FindElement(By.Id("6dropBtn"));
-            unitTestCourse.Click();
-
-            System.Threading.Thread.Sleep(4000); //Sleep for 4 seconds so we can see that the unit test course has been registered
+			System.Threading.Thread.Sleep(4000); //Sleep for 4 seconds so we can see that the unit test course has been registered
 
 
-            driver.Quit();
+			driver.Quit();
 
         }
     }
